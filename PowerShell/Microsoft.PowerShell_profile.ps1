24988eb142437d87c40b742bd649a48a75b89696
denv3rr <denver.clark@g.austincc.edu>
2024-10-17 15:16:45 -0500

    edited pwsh script to just run neofetch command at the start of terminal profile

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,7 @@
+function Run-Neofetch {
+    & "C:\Program Files\Git\bin\bash.exe" -c "neofetch"
+}
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\night-owl.omp.json" | Invoke-Expression
+
+Run-Neofetch
